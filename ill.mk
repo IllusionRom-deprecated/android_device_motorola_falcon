4ad41f089701d784ea930d37c1edbf60d0293f38
herna <hernanderes34@gmail.com>
2014-03-15 23:23:53 +0100

    Illusion Bring Up for Moto G

---
+++ ill.mk
@@ -0,0 +1,20 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+PRODUCT_RELEASE_NAME := Moto G
+
+$(call inherit-product, device/motorola/falcon/full_falcon.mk)
+
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := falcon
+PRODUCT_NAME := ill_falcon
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := Moto G
+PRODUCT_MANUFACTURER := Motorola
+
+# bootanimation
+PRODUCT_COPY_FILES += \
+        vendor/illusion/bootanimation/Illusion720x720.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo cm_falcon-userdebug
+add_lunch_combo ill_falcon-userdebug
 
